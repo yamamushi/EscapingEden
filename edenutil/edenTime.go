b1742b67c2e1e2c8817e831ecc7cc7ab11f758dd
yamamushi <yamamushi@gmail.com>
2023-10-18 22:52:56 -0600

    Update Time functions

--- edenutil/edenTime.go
+++ edenutil/edenTime.go
@@ -31,6 +31,7 @@ const (
 	EdenMonthTerezi
 	EdenMonthVriska
 	EdenMonthEquius
+	EdenMonthNewMonth // Adding a new month
 )
 
 // String returns a string representation of the EdenMonth.
@@ -72,6 +73,8 @@ func (em EdenMonth) String() string {
 		return "Vriska"
 	case EdenMonthEquius:
 		return "Equius"
+	case EdenMonthNewMonth:
+		return "NewMonth" // Random name for the new month
 	default:
 		return ""
 	}
@@ -134,35 +137,35 @@ func (ed EdenDay) String() string {
 type EdenTime struct {
 }
 
-// NanoSecond Returns the nanoseconds since the creation of the Eden Universe.
+// NanoSecond returns the nanoseconds since the creation of the Eden Universe.
 func (et EdenTime) NanoSecond() int64 {
 	// Current nanoseconds since 12:00AM January 1st, 2021 UTC
 	nanoSecond := time.Since(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)).Nanoseconds()
 	return nanoSecond
 }
 
-// MicroSecond Returns the microseconds since the creation of the Eden Universe.
+// MicroSecond returns the microseconds since the creation of the Eden Universe.
 func (et EdenTime) MicroSecond() int64 {
 	// Current microseconds since 12:00AM January 1st, 2021 UTC
 	microSecond := time.Since(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)).Microseconds()
 	return microSecond
 }
 
-// Second Returns the seconds since the creation of the Eden Universe.
+// Second returns the seconds since the creation of the Eden Universe.
 func (et EdenTime) Second() int64 {
 	// Current seconds since 12:00AM January 1st, 2021 UTC
 	second := int64(time.Since(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)).Seconds())
 	return second
 }
 
-// Minute Returns the minutes since the creation of the Eden Universe.
+// Minute returns the minutes since the creation of the Eden Universe.
 func (et EdenTime) Minute() int64 {
 	// Current minutes since 12:00AM January 1st, 2021 UTC
 	minute := int64(time.Since(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)).Minutes())
 	return minute
 }
 
-// Hour Returns the hours since the creation of the Eden Universe.
+// Hour returns the hours since the creation of the Eden Universe.
 func (et EdenTime) Hour() int64 {
 	// Current hours since 12:00AM January 1st, 2021 UTC
 	hour := int64(time.Since(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)).Hours())
@@ -173,7 +176,7 @@ func (et EdenTime) Hour() int64 {
 // A day is broken into 27 hours, with an hour being broken into 60 minutes, a minute 60 seconds, etc.
 func (et EdenTime) Day() int64 {
 	// Current days since 12:00AM January 1st, 2021 UTC
-	day := int64(time.Since(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)).Hours() / 24)
+	day := int64(time.Since(time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)).Hours() / 27)
 	return day
 }
 
@@ -198,27 +201,27 @@ func (et EdenTime) Month() int64 {
 // A year is broken into 18 months.
 func (et EdenTime) Year() int64 {
 	// Current years since 12:00AM January 1st, 2021 UTC
-	year := et.Month() / 18
+	year := et.Month() / 18 // Add 1 to start from year 1
 	return year
 }
 
 // EdenTimestamp returns the current hour, minute and second formatted as HH:MM:SS
-func (et EdenTime) EdenTimestamp() (second int64, minute int64, hour int64) {
+func (et EdenTime) EdenTimestamp() (hour int64, minute int64, second int64) {
 	// Current hour, minute and second since 12:00AM January 1st, 2021 UTC
 	second = et.Second()
-	minute = (second / 60)
-	hour = (minute / 60)
-	return second % 60, minute % 60, hour % 27
+	minute = second / 60
+	hour = minute / 27
+	return hour % 27, minute % 60, second % 60
 }
 
-// EdenMonth Returns the current EdenMonth
+// EdenMonth returns the current EdenMonth
 func (et EdenTime) EdenMonth() EdenMonth {
 	// The current month of the Eden Calendar
 	month := et.Month() % 18
 	return EdenMonth(month)
 }
 
-// EdenDay Returns the current EdenDay
+// EdenDay returns the current EdenDay
 func (et EdenTime) EdenDay() EdenDay {
 	// The current day of the Eden Calendar
 	day := et.Day() % 12
@@ -234,12 +237,12 @@ func (et EdenTime) EdenDayString() string {
 
 // CurrentTimeString returns the current time formatted as HH:MM:SS
 func (et EdenTime) CurrentTimeString() string {
-	second, minute, hour := et.EdenTimestamp()
+	hour, minute, second := et.EdenTimestamp()
 	return fmt.Sprintf("%02d:%02d:%02d", hour, minute, second)
 }
 
-// TimeStamp String returns the current day time formatted as "<day> of the <week> Week, <month> in the Year <year> - HH:MM:SS"
-func (et EdenTime) TimeStamp() string {
+// TimeStampString returns the current day time formatted as "<day> of the <week> Week, <month> in the Year <year> - HH:MM:SS"
+func (et EdenTime) TimeStampString() string {
 	// The current day of the Eden Calendar
 	day := et.EdenDay().String()
 	// The current week of the Eden Calendar
@@ -249,7 +252,7 @@ func (et EdenTime) TimeStamp() string {
 	// The current year of the Eden Calendar
 	year := et.Year()
 	// The current hour, minute and second since 12:00AM January 1st, 2021 UTC
-	second, minute, hour := et.EdenTimestamp()
+	hour, minute, second := et.EdenTimestamp()
 
 	return fmt.Sprintf("%s of Week %d in %s, Year %d - %02d:%02d:%02d", day, week, month, year, hour, minute, second)
 }
