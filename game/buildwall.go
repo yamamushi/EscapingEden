3bf159295d0163e8b71e9b2a3240d0aaf2dfd642
yamamushi <yamamushi@gmail.com>
2023-11-01 17:40:58 -0600

    Fix cross-boundry wall drawing

--- game/buildwall.go
+++ game/buildwall.go
@@ -3,6 +3,7 @@ package game
 import (
 	"errors"
 	"github.com/yamamushi/EscapingEden/edentypes"
+	"github.com/yamamushi/EscapingEden/logging"
 	"log"
 	"strings"
 )
@@ -35,7 +36,7 @@ func (gm *GameManager) HandleBuildWallRequest(itemID string, toolID string, char
 		log.Println("Failed to get character", err.Error())
 		return errors.New("character not found")
 	}
-	tile := gm.GetTileFromCharacter(charID, character.Position.X+deltaX, character.Position.Y+deltaY, 0)
+	tile, _, _, _ := gm.GetTileFromCharacter(charID, character.Position.X+deltaX, character.Position.Y+deltaY, 0)
 	if tile == nil {
 		//log.Println("Tile not found")
 		return errors.New("tile not found")
@@ -46,13 +47,17 @@ func (gm *GameManager) HandleBuildWallRequest(itemID string, toolID string, char
 	}
 	tile.TileType = wallTypePrefix
 	// Now we want to check the surrounding to tiles to see if any are also walls
-	gm.FixWallAlignment(charID, character.Position.X+deltaX, character.Position.Y+deltaY, 0, true)
+	gm.FixWallAlignment(charID, character.Position.X+deltaX, character.Position.Y+deltaY, 0, 3)
 	return nil
 }
 
-func (gm *GameManager) FixWallAlignment(charID string, x, y, z int, recurse bool) {
+func (gm *GameManager) FixWallAlignment(charID string, x, y, z int, recurseCount int) {
+	if recurseCount <= 0 {
+		return
+	}
+	recurseCount -= 1
 	//gm.Log.Println(logging.LogInfo, "Fixing wall alignment")
-	thisTile := gm.GetTileFromCharacter(charID, x, y, z)
+	thisTile, x, y, z := gm.GetTileFromCharacter(charID, x, y, z)
 	tileVars := strings.Split(thisTile.TileType, "_")
 	if len(tileVars) < 2 {
 		return
@@ -61,7 +66,14 @@ func (gm *GameManager) FixWallAlignment(charID string, x, y, z int, recurse bool
 		return
 	}
 	thisTile.TileType = strings.Split(thisTile.TileType, "_")[0]
-	n, _, e, _, s, _, w, _ := gm.GetSurroundingTiles(charID, x, y, z)
+	character, err := gm.GetCharacter(charID)
+	if err != nil {
+		gm.Log.Println(logging.LogInfo, "Failed to get character", err.Error())
+		return
+	}
+	mapChunk := gm.GetMapChunkByID(character.CurrentMapID)
+
+	n, _, e, _, s, _, w, _ := gm.GetSurroundingTiles(mapChunk, x, y, z)
 	pN, pE, pS, pW := false, false, false, false
 	// Now we're going to walk through the surrounding tiles and check if they are walls
 	// North
@@ -84,126 +96,94 @@ func (gm *GameManager) FixWallAlignment(charID string, x, y, z int, recurse bool
 
 	if pN && !pE && !pS && !pW {
 		thisTile.TileType += "_wall_vertical_north"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y-1, z, false)
-		}
+		gm.FixWallAlignment(charID, x, y-1, z, recurseCount)
 		return
 	}
 	if !pN && pE && !pS && !pW {
 		thisTile.TileType += "_wall_horizontal_east"
-		if recurse {
-			gm.FixWallAlignment(charID, x+1, y, z, false)
-		}
+		gm.FixWallAlignment(charID, x+1, y, z, recurseCount)
 		return
 	}
 	if !pN && !pE && pS && !pW {
 		thisTile.TileType += "_wall_vertical_south"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y+1, z, false)
-		}
+		gm.FixWallAlignment(charID, x, y+1, z, recurseCount)
 		return
 	}
 	if pN && !pE && pS && !pW {
 		thisTile.TileType += "_wall_vertical"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y-1, z, false) // North
-			gm.FixWallAlignment(charID, x, y+1, z, false) // South
-		}
+		gm.FixWallAlignment(charID, x, y-1, z, recurseCount) // North
+		gm.FixWallAlignment(charID, x, y+1, z, recurseCount) // South
 		return
 	}
 	if !pN && !pE && !pS && pW {
 		thisTile.TileType += "_wall_horizontal_west"
-		if recurse {
-			gm.FixWallAlignment(charID, x-1, y, z, false) // West
-		}
+		gm.FixWallAlignment(charID, x-1, y, z, recurseCount) // West
 		return
 	}
 	if !pN && pE && !pS && pW {
 		thisTile.TileType += "_wall_horizontal"
-		if recurse {
-			gm.FixWallAlignment(charID, x+1, y, z, false) // East
-			gm.FixWallAlignment(charID, x-1, y, z, false) // West
-
-		}
+		gm.FixWallAlignment(charID, x+1, y, z, recurseCount) // East
+		gm.FixWallAlignment(charID, x-1, y, z, recurseCount) // West
 		return
 	}
 	if pN && pE && !pS && !pW {
 		thisTile.TileType += "_wall_bottom_left"
-		if recurse {
-			gm.FixWallAlignment(charID, x+1, y, z, false) // East
-			gm.FixWallAlignment(charID, x, y-1, z, false) // North
-		}
+		gm.FixWallAlignment(charID, x+1, y, z, recurseCount) // East
+		gm.FixWallAlignment(charID, x, y-1, z, recurseCount) // North
 		return
 	}
 	if pN && !pE && !pS && pW {
 		thisTile.TileType += "_wall_bottom_right"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y-1, z, false) // North
-			gm.FixWallAlignment(charID, x-1, y, z, false) // West
-
-		}
+		gm.FixWallAlignment(charID, x, y-1, z, recurseCount) // North
+		gm.FixWallAlignment(charID, x-1, y, z, recurseCount) // West
 		return
 	}
 	if !pN && pE && pS && !pW {
 		thisTile.TileType += "_wall_top_left"
-		if recurse {
-			gm.FixWallAlignment(charID, x+1, y, z, false) // East
-			gm.FixWallAlignment(charID, x, y+1, z, false) // South
-		}
+		gm.FixWallAlignment(charID, x+1, y, z, recurseCount) // East
+		gm.FixWallAlignment(charID, x, y+1, z, recurseCount) // South
 		return
 	}
 	if !pN && !pE && pS && pW {
 		thisTile.TileType += "_wall_top_right"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y+1, z, false) // South
-			gm.FixWallAlignment(charID, x-1, y, z, false) // West
-		}
+		gm.FixWallAlignment(charID, x, y+1, z, recurseCount) // South
+		gm.FixWallAlignment(charID, x-1, y, z, recurseCount) // West
 		return
 	}
 	if pN && pE && pS && !pW {
 		thisTile.TileType += "_wall_north_east_south"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y-1, z, false) // North
-			gm.FixWallAlignment(charID, x+1, y, z, false) // East
-			gm.FixWallAlignment(charID, x, y+1, z, false) // South
-		}
+		gm.FixWallAlignment(charID, x, y-1, z, recurseCount) // North
+		gm.FixWallAlignment(charID, x+1, y, z, recurseCount) // East
+		gm.FixWallAlignment(charID, x, y+1, z, recurseCount) // South
 		return
 	}
 	if pN && !pE && pS && pW {
 		thisTile.TileType += "_wall_north_west_south"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y-1, z, false) // North
-			gm.FixWallAlignment(charID, x-1, y, z, false) // West
-			gm.FixWallAlignment(charID, x, y+1, z, false) // South
-		}
+		gm.FixWallAlignment(charID, x, y-1, z, recurseCount) // North
+		gm.FixWallAlignment(charID, x-1, y, z, recurseCount) // West
+		gm.FixWallAlignment(charID, x, y+1, z, recurseCount) // South
 		return
 	}
 	if !pN && pE && pS && pW {
 		thisTile.TileType += "_wall_south_east_west"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y+1, z, false) // South
-			gm.FixWallAlignment(charID, x+1, y, z, false) // East
-			gm.FixWallAlignment(charID, x-1, y, z, false) // West
-		}
+		gm.FixWallAlignment(charID, x, y+1, z, recurseCount) // South
+		gm.FixWallAlignment(charID, x+1, y, z, recurseCount) // East
+		gm.FixWallAlignment(charID, x-1, y, z, recurseCount) // West
 		return
 	}
 	if pN && pE && !pS && pW {
 		thisTile.TileType += "_wall_north_east_west"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y-1, z, false) // North
-			gm.FixWallAlignment(charID, x+1, y, z, false) // East
-			gm.FixWallAlignment(charID, x-1, y, z, false) // West
-		}
+		gm.FixWallAlignment(charID, x, y-1, z, recurseCount) // North
+		gm.FixWallAlignment(charID, x+1, y, z, recurseCount) // East
+		gm.FixWallAlignment(charID, x-1, y, z, recurseCount) // West
 		return
 	}
 	if pN && pE && pS && pW {
 		thisTile.TileType += "_wall_north_east_south_west"
-		if recurse {
-			gm.FixWallAlignment(charID, x, y-1, z, false) // North
-			gm.FixWallAlignment(charID, x+1, y, z, false) // East
-			gm.FixWallAlignment(charID, x, y+1, z, false) // South
-			gm.FixWallAlignment(charID, x-1, y, z, false) // West
-		}
+		gm.FixWallAlignment(charID, x, y-1, z, recurseCount) // North
+		gm.FixWallAlignment(charID, x+1, y, z, recurseCount) // East
+		gm.FixWallAlignment(charID, x, y+1, z, recurseCount) // South
+		gm.FixWallAlignment(charID, x-1, y, z, recurseCount) // West
 		return
 	}
 
--- game/gettile.go
+++ game/gettile.go
@@ -1,12 +1,14 @@
 package game
 
-import "github.com/yamamushi/EscapingEden/logging"
+import (
+	"github.com/yamamushi/EscapingEden/logging"
+)
 
-func (gm *GameManager) GetTileFromCharacter(charID string, x, y, z int) *Tile {
+func (gm *GameManager) GetTileFromCharacter(charID string, x, y, z int) (tile *Tile, X, Y, Z int) {
 	character, err := gm.GetCharacter(charID)
 	if err != nil {
 		gm.Log.Println(logging.LogError, "Failed to get character", err.Error())
-		return nil
+		return nil, 0, 0, 0
 	}
 
 	targetMap := gm.GetMapChunkByID(character.CurrentMapID)
@@ -30,128 +32,66 @@ func (gm *GameManager) GetTileFromCharacter(charID string, x, y, z int) *Tile {
 		}
 
 		targetMap = gm.GetMapChunkFrom(targetMap, dX, dY, 0)
-		return &targetMap.TileMap[x][y][z]
+		return &targetMap.TileMap[x][y][z], x, y, z
 
 	} else {
 		// Get the tile at the requested position
-		return &targetMap.TileMap[x][y][z]
+		return &targetMap.TileMap[x][y][z], x, y, z
 	}
-
-	return nil
 }
 
-func (gm *GameManager) GetSurroundingTiles(charID string, x, y, z int) (*Tile, *Tile, *Tile, *Tile, *Tile, *Tile, *Tile, *Tile) {
-	character, err := gm.GetCharacter(charID)
-	if err != nil {
-		gm.Log.Println(logging.LogError, "Failed to get character", err.Error())
-		return nil, nil, nil, nil, nil, nil, nil, nil
-	}
+func (gm *GameManager) GetSurroundingTiles(tileMap *MapChunk, x, y, z int) (*Tile, *Tile, *Tile, *Tile, *Tile, *Tile, *Tile, *Tile) {
 
 	n, ne, e, se, s, sw, w, nw := &Tile{}, &Tile{}, &Tile{}, &Tile{}, &Tile{}, &Tile{}, &Tile{}, &Tile{}
-
-	targetMap := gm.GetMapChunkByID(character.CurrentMapID)
-	// North
-	if y-1 < 0 {
-		nMap := gm.GetMapChunkFrom(targetMap, 0, -1, 0)
-		n = &nMap.TileMap[x][len(nMap.TileMap[0])-1][z]
-	} else {
-		n = &targetMap.TileMap[x][y-1][z]
-	}
-
-	// North East
-	if y-1 < 0 {
-		if x+1 > len(targetMap.TileMap)-1 {
-			neMap := gm.GetMapChunkFrom(targetMap, 1, -1, 0)
-			ne = &neMap.TileMap[0][len(neMap.TileMap[0])-1][z]
-		} else {
-			neMap := gm.GetMapChunkFrom(targetMap, 1, -1, 0)
-			ne = &neMap.TileMap[x+1][len(neMap.TileMap[0])-1][z]
-		}
-	} else {
-		if x+1 > len(targetMap.TileMap)-1 {
-			neMap := gm.GetMapChunkFrom(targetMap, 1, 0, 0)
-			ne = &neMap.TileMap[0][y-1][z]
-		} else {
-			ne = &targetMap.TileMap[x+1][y-1][z]
-		}
-	}
-
-	// East
-	if x+1 > len(targetMap.TileMap)-1 {
-		eMap := gm.GetMapChunkFrom(targetMap, 1, 0, 0)
-		e = &eMap.TileMap[0][y][z]
-	} else {
-		e = &targetMap.TileMap[x+1][y][z]
-	}
-
-	// South East
-	if y+1 > len(targetMap.TileMap[0])-1 {
-		if x+1 > len(targetMap.TileMap)-1 {
-			seMap := gm.GetMapChunkFrom(targetMap, 1, 1, 0)
-			se = &seMap.TileMap[0][0][z]
-		} else {
-			seMap := gm.GetMapChunkFrom(targetMap, 1, 1, 0)
-			se = &seMap.TileMap[x+1][0][z]
-		}
-	} else {
-		if x+1 > len(targetMap.TileMap)-1 {
-			seMap := gm.GetMapChunkFrom(targetMap, 1, 0, 0)
-			se = &seMap.TileMap[0][y+1][z]
-		} else {
-			se = &targetMap.TileMap[x+1][y+1][z]
-		}
-	}
-
-	// South
-	if y+1 > len(targetMap.TileMap[0])-1 {
-		sMap := gm.GetMapChunkFrom(targetMap, 0, 1, 0)
-		s = &sMap.TileMap[x][0][z]
-	} else {
-		s = &targetMap.TileMap[x][y+1][z]
-	}
-
-	// South West
-	if y+1 > len(targetMap.TileMap[0])-1 {
-		if x-1 < 0 {
-			swMap := gm.GetMapChunkFrom(targetMap, -1, 1, 0)
-			sw = &swMap.TileMap[len(swMap.TileMap)-1][0][z]
-		} else {
-			swMap := gm.GetMapChunkFrom(targetMap, -1, 1, 0)
-			sw = &swMap.TileMap[x-1][0][z]
-		}
-	} else {
-		if x-1 < 0 {
-			swMap := gm.GetMapChunkFrom(targetMap, -1, 0, 0)
-			sw = &swMap.TileMap[len(swMap.TileMap)-1][y+1][z]
-		} else {
-			sw = &targetMap.TileMap[x-1][y+1][z]
-		}
-	}
-
-	// West
-	if x-1 < 0 {
-		wMap := gm.GetMapChunkFrom(targetMap, -1, 0, 0)
-		w = &wMap.TileMap[len(wMap.TileMap)-1][y][z]
-	} else {
-		w = &targetMap.TileMap[x-1][y][z]
-	}
-
-	// North West
-	if y-1 < 0 {
-		if x-1 < 0 {
-			nwMap := gm.GetMapChunkFrom(targetMap, -1, -1, 0)
-			nw = &nwMap.TileMap[len(nwMap.TileMap)-1][len(nwMap.TileMap[0])-1][z]
-		} else {
-			nwMap := gm.GetMapChunkFrom(targetMap, -1, -1, 0)
-			nw = &nwMap.TileMap[x-1][len(nwMap.TileMap[0])-1][z]
-		}
-	} else {
-		if x-1 < 0 {
-			nwMap := gm.GetMapChunkFrom(targetMap, -1, 0, 0)
-			nw = &nwMap.TileMap[len(nwMap.TileMap)-1][y-1][z]
-		} else {
-			nw = &targetMap.TileMap[x-1][y-1][z]
-		}
-	}
+	
+	// Cover the top row first, gathering a new map chunk if necessary
+	n = gm.GetNorthTile(tileMap, x, y, z)
+	e = gm.GetEastTile(tileMap, x, y, z)
+	s = gm.GetSouthTile(tileMap, x, y, z)
+	w = gm.GetWestTile(tileMap, x, y, z)
 	return n, ne, e, se, s, sw, w, nw
+}
+
+// Returns the tile north of the given position
+func (gm *GameManager) GetNorthTile(tileMap *MapChunk, x, y, z int) *Tile {
+	if y == 0 {
+		// Get the map chunk north of the current one
+		mapChunk := gm.GetMapChunkFrom(tileMap, 0, -1, 0)
+		return &mapChunk.TileMap[x][len(mapChunk.TileMap[0])-1][z]
+	} else {
+		return &tileMap.TileMap[x][y-1][z]
+	}
+}
+
+// Returns the tile east of the given position
+func (gm *GameManager) GetEastTile(tileMap *MapChunk, x, y, z int) *Tile {
+	if x == len(tileMap.TileMap)-1 {
+		// Get the map chunk east of the current one
+		mapChunk := gm.GetMapChunkFrom(tileMap, 1, 0, 0)
+		return &mapChunk.TileMap[0][y][z]
+	} else {
+		return &tileMap.TileMap[x+1][y][z]
+	}
+}
+
+// Returns the tile south of the given position
+func (gm *GameManager) GetSouthTile(tileMap *MapChunk, x, y, z int) *Tile {
+	if y == len(tileMap.TileMap[0])-1 {
+		// Get the map chunk south of the current one
+		mapChunk := gm.GetMapChunkFrom(tileMap, 0, 1, 0)
+		return &mapChunk.TileMap[x][0][z]
+	} else {
+		return &tileMap.TileMap[x][y+1][z]
+	}
+}
+
+// Returns the tile west of the given position
+func (gm *GameManager) GetWestTile(tileMap *MapChunk, x, y, z int) *Tile {
+	if x == 0 {
+		// Get the map chunk west of the current one
+		mapChunk := gm.GetMapChunkFrom(tileMap, -1, 0, 0)
+		return &mapChunk.TileMap[len(mapChunk.TileMap)-1][y][z]
+	} else {
+		return &tileMap.TileMap[x-1][y][z]
+	}
 }
